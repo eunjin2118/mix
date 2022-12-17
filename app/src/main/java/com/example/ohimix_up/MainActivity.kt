a3b594a3cf7c780a94259e1e99082e3da37015d8
Bae Seoyeon <s2103@e-mirim.hs.kr>
2022-12-17 17:43:46 +0900

    nav, fragment 연결

--- app/src/main/java/com/example/ohimix_up/MainActivity.kt
+++ app/src/main/java/com/example/ohimix_up/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.ohimix_up
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
+
+        bnv_main.run {
+            setOnNavigationItemReselectedListener {
+                when(it.itemId){
+                    R.id.item_fragment1 -> {
+                        val Home = HomeFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container,Home).commit()
+                    }
+                    R.id.item_fragment2 -> {
+                        val  Quiz = QuizFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, Quiz
+                        ).commit()
+                    }
+                    R.id.item_fragment3 -> {
+                        val Community = CommunityFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, Community).commit()
+                    }
+                    R.id.item_fragment4 -> {
+                        val User = UserFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container, User).commit()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.item_fragment1
+        }
+
+
+
     }
 }
