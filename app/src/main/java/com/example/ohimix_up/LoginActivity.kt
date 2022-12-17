8c37732b643ee3773476b1a718c99bb28f5af2b4
편은진 <ally6324@naver.com>
2022-12-17 23:54:48 +0900

    로그인 회원가입

---
+++ app/src/main/java/com/example/ohimix_up/LoginActivity.kt
@@ -0,0 +1,62 @@
+package com.example.ohimix_up
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    private  lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+        auth = Firebase.auth
+
+        val signInBtn = findViewById<Button>(R.id.loginBtn)
+
+        val currentUser = auth.currentUser
+        if(currentUser != null) {
+            // 로그인된 사용자가 있는 경우 필요한 작업 진행 (ex: 그냥 로그인 화면 넘어가서 메인 화면으로 가기)
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        signInBtn.setOnClickListener {
+            val email = findViewById<EditText>(R.id.loginEmail).text.toString()
+            val password = findViewById<EditText>(R.id.loginPW).text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if(it.isSuccessful){
+                        // 성공
+                        Log.d("mytag", "로그인 성공 ${it.toString()}")
+                        Toast.makeText(baseContext, "로그인 셩굥", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        Log.w("mytag", "로그인 실패 (사유 : ${it.exception})")
+                        Toast.makeText(baseContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+
+                        signInBtn.isEnabled = true // 버튼 다시 누르기
+                    }
+                }
+        }
+
+//        findViewById<Button>(R.id.signUpBtn).setOnClickListener {
+//            val intent = Intent(this, SignUpActivity::class.java)
+//            startActivity(intent)
+//        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/ohimix_up/SignUpActivity.kt
@@ -0,0 +1,74 @@
+package com.example.ohimix_up
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        auth = Firebase.auth
+        val signUpBtn = findViewById<Button>(R.id.btnSignup)
+
+        val db = Firebase.firestore
+
+        supportActionBar?.hide()
+
+        signUpBtn.setOnClickListener {
+            val email = findViewById<EditText>(R.id.email_input).text.toString()
+            val password = findViewById<EditText>(R.id.password_input).text.toString()
+            val passwordCheck = findViewById<EditText>(R.id.password_check).text.toString()
+            val nickname = findViewById<EditText>(R.id.nickname_input).text.toString()
+
+            val user = hashMapOf(
+                "nickname" to nickname,
+                "email" to email,
+                "password" to password
+            )
+
+            Log.d("mytag", "회원가입 성공 ${user.toString()}")
+
+            db.collection("users").document(email)
+                .set(user)
+                .addOnSuccessListener {
+                    Log.d("mytag", "DocumentSnapshot successfully written!")
+                }
+                .addOnFailureListener{
+                        e -> Log.d("mytag", "Error writing document", e)
+                }
+
+            signUpBtn.isEnabled = false
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "회원가입 되었습니다", Toast.LENGTH_SHORT).show()
+                        val user = auth.currentUser
+                        Log.d("mytag", "회원가입 성공 ${user.toString()}")
+                        finish()
+                        val intent = Intent(this, HomeFragment::class.java)
+                        startActivity(intent)
+                    } else {
+                        Log.w("mytag", "회원 가입 실패 (사유 : ${it.exception})")
+                        Toast.makeText(baseContext, "회원 가입 실패", Toast.LENGTH_SHORT).show()
+                        signUpBtn.isEnabled = true // 버튼 다시 누르기
+                    }
+                }
+        }
+
+
+    }
+}
